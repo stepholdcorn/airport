baf5cf5e050712b956ea06859364f87befabd638
stepholdcorn <stephieoldcorn@gmail.com>
2014-12-12 17:30:43 +0000

    Adding the capacity to the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,15 +1,30 @@
 class Airport
 
-	def initialize
-		@plane
+	DEFAULT_CAPACITY = 10
+
+	attr_writer :capacity
+
+	def initialize(options = {})
+		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
+		@planes = []
 	end 
+
+	def capacity
+		@capacity ||= DEFAULT_CAPACITY
+	end
+
+	def plane_count
+		@planes.count
+	end
 
 	def dispatch_plane(plane)
 		plane.dispatch!
+		@planes.delete(plane)
 	end
 
 	def land_plane(plane)
 		plane.land!
+		@planes << plane
 	end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,21 +1,41 @@
 require 'airport'
 
 describe Airport do 
-	let(:airport) {Airport.new}
+	let(:airport) {Airport.new(:capacity => 12)}
 	let(:plane) {double :plane}
 
-context 'dispatching and landing planes' do
+	context 'dispatching and landing planes' do
 
-it 'should be able to dispatch a plane' do
-	expect(plane).to receive(:dispatch!)
-	airport.dispatch_plane(plane)
-end
+		it 'should be able to dispatch a plane' do
+			expect(plane).to receive(:dispatch!)
+			airport.dispatch_plane(plane)
+		end
 
-it 'should be able to land a plane' do
-	expect(plane).to receive(:land!)
-	airport.land_plane(plane)
-end
+		it 'should reduce the plane count when a plane has been dispatched' do
+			expect(plane).to receive(:dispatch!)
+			airport.dispatch_plane(plane)
+			expect(airport.plane_count).to eq(0)
+		end
 
-end
+		it 'should be able to land a plane' do
+			expect(plane).to receive(:land!)
+			airport.land_plane(plane)
+		end
+
+		it 'should increase the plane count when a plane has landed' do
+			expect(plane).to receive(:land!)
+			airport.land_plane(plane)
+			expect(airport.plane_count).to eq(1)
+		end
+
+	end
+
+	context 'traffic control' do
+
+		it 'should allow setting of default capacity when initialized' do
+			expect(airport.capacity).to eq(12)
+		end
+
+	end
 
 end
